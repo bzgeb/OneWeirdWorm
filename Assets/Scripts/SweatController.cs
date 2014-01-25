48e65ec635adb685f7434bb5af5919cb955ec74d
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-01-25 15:24:39 -0500

    Added sweat

---
+++ Assets/Scripts/SweatController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SweatController : MonoBehaviour 
+{
+    public float sweatTime;
+    float elapsedTime;
+
+    void Update() {
+        elapsedTime += Time.deltaTime;
+
+        if ( elapsedTime > sweatTime ) {
+            particleSystem.Emit( 5 );
+            elapsedTime = 0;
+        }
+    }
+}
